0bbe494c07436f244466bc8dc87b7185273a26df
Tran Thanh <mrthanhbeo@gmail.com>
2021-08-18 13:42:02 +0700

    this push from branch-b

--- src/mainclass/Main.java
+++ src/mainclass/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This is Master");
-		System.out.println("This was merge from branch-a");
+		System.out.println("This was merge from branch-b");
 
 	}
 
